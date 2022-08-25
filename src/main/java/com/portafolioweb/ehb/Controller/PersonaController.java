be36ec62e941588533d7041a4c18be8af56ab437
EdwardHB <hedward1998hb@gmail.com>
2022-08-24 21:01:40 -0300

    be save

--- src/main/java/com/portafolioweb/ehb/Controller/PersonaController.java
+++ src/main/java/com/portafolioweb/ehb/Controller/PersonaController.java
@@ -8,6 +8,7 @@ import com.portafolioweb.ehb.Entity.Persona;
 import com.portafolioweb.ehb.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author eduen
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -50,6 +52,11 @@ public class PersonaController {
         persona.setImg(nuevoImg);
         ipersonaService.guardarPersona(persona);
         return persona;
+    }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona encontrarPersona(){
+        return ipersonaService.encontrarPersona((long)1);
     }
     
 }
